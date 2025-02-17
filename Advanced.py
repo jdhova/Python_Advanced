710d604465ca2d7cd587b4f27e6908967e8422d6
jdhova <juud0010@gmail.com>
2025-02-17 14:50:33 -0400

    f

---
+++ Advanced.py
@@ -0,0 +1,17 @@
+list  = ['Dave','James','Adam','Kate','James']
+print(list)
+
+mysets = set(list)
+print(mysets)
+mysets.add('Grace')
+print((mysets))
+mysets.add('Grace')
+print(mysets)
+
+
+
+
+
+# my_set = {1, 2, 3, 4, 5}
+# my_list = list(my_set)
+# print(my_list)
